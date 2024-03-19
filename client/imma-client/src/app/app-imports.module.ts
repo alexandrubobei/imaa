a045a70042d069bdb94e4ddf9f9f13f00a56eafd
Adrian <tocu.adrian17@gmail.com>
2024-03-19 12:34:02 +0200

    Refactor and add investment routes and page

--- client/imma-client/src/app/app-imports.module.ts
+++ client/imma-client/src/app/app-imports.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { MatIcon } from '@angular/material/icon';
 import { MatSidenavModule } from '@angular/material/sidenav';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 
-const modules = [MatIcon, MatSidenavModule];
+const modules = [
+  RouterOutlet,
+  RouterLink,
+  RouterLinkActive,
+  MatIcon,
+  MatSidenavModule,
+];
 
 @NgModule({
   declarations: [],
---
+++ client/imma-client/src/app/modules/investments/pages/investment-listview/investment-listview.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-investment-listview',
+  standalone: true,
+  imports: [],
+  templateUrl: './investment-listview.component.html',
+  styleUrl: './investment-listview.component.scss'
+})
+export class InvestmentListviewComponent {
+
+}
